317219abc708d27749bdd4f4fe16b16b48526a88
Chauncey McAskill <chauncey@mcaskill.ca>
2019-04-02 22:43:47 -0400

    Draft #1
    Added:
    - Favicons
    - Index
    - List of occupations

---
+++ src/app.php
@@ -0,0 +1,558 @@
+<?php
+
+class Value
+{
+    /**
+     * The primitive value.
+     *
+     * @var mixed
+     */
+    private $data;
+
+    /**
+     * The locales of the primitive value.
+     *
+     * @var string[]
+     */
+    private $locales;
+
+    /**
+     * @param mixed $data The primitive value.
+     */
+    public function __construct($data)
+    {
+        if (is_object($data) && (property_exists($data, '@en') || property_exists($data, '@fr'))) {
+            $data = get_object_vars($data);
+        } else {
+            $data = [
+                '@en' => $data,
+            ];
+        }
+
+        $this->data    = $data;
+        $this->locales = array_keys($data);
+    }
+
+    /**
+     * Retrieves a string representing the value.
+     *
+     * @return mixed
+     */
+    public function __toString()
+    {
+        if ($this->locales) {
+            $key = $this->locales[0];
+            if (isset($this->data[$key])) {
+                return $this->data[$key];
+            }
+        }
+
+        return '';
+    }
+
+    /**
+     * Retrieves a string representing the value.
+     *
+     * This method is meant to be overridden by derived objects for locale-specific purposes.
+     *
+     * @param  mixed $locales Optional.
+     *     A string with a BCP 47 language tag, or an array of such strings.
+     * @array  array $options Optional.
+     *     An object with some or all of the following properties:
+     * @return string|null A string with a language-sensitive representation of the given number.
+     */
+    public function toLocaleString($locales = null, array $options = [])
+    {
+        $locales = (array) $locales;
+        foreach ($locales as $locale) {
+            $key = '@'.$locale;
+            if (isset($this->data[$key])) {
+                return $this->data[$key];
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Retrieves a string representing the BCP 47 language tag of the value.
+     *
+     * @return string|null
+     */
+    public function localeOf()
+    {
+        return $this->locales ? substr($this->locales[0], 1) : null;
+    }
+
+    /**
+     * @param  mixed $data The primitive value.
+     * @return ?static
+     */
+    public static function create($data)
+    {
+        if ($data !== null) {
+            $data = new static($data);
+        }
+
+        return $data;
+    }
+}
+
+class DateRangeCollection
+{
+    /**
+     * @var DateRange[]
+     */
+    private $ranges = [];
+
+    /**
+     * @param  array $collection
+     * @throws InvalidArgumentException if no ranges are given.
+     */
+    public function __construct(...$collection)
+    {
+        if (count($collection) === 0) {
+            throw new InvalidArgumentException('Expected at least one date range');
+        }
+
+        foreach ($collection as $range) {
+            $this->ranges[] = DateRange::create($range);
+        }
+
+        usort($this->ranges, function (DateRange $a, DateRange $b) {
+            return DateRange::compare($a, $b);
+        });
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string)$this->getRange();
+    }
+
+    /**
+     * @param  array $ranges
+     * @return static
+     */
+    public function add(array $ranges)
+    {
+        return new static(
+            array_merge(
+                $this->ranges,
+                $ranges
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getFrom()
+    {
+        return reset($this->ranges)->getFrom();
+    }
+
+    /**
+     * @return string
+     */
+    public function getUntil()
+    {
+        return end($this->ranges)->getUntil();
+    }
+
+    /**
+     * @return DateRange
+     */
+    public function getRange()
+    {
+        return DateRange::create([ $this->getFrom(), $this->getUntil() ]);
+    }
+}
+
+class DateRange
+{
+    const REGEX_DATE   = '/^(?<Y>\d{4})(?:-(?<m>\d{2})(?:-(?<d>\d{2}))?)?$/';
+    const OPEN_DATE    = '..';
+    const UNKNOWN_DATE = '.';
+
+    /**
+     * @var string
+     */
+    private $from;
+
+    /**
+     * @var string
+     */
+    private $until;
+
+    /**
+     * @param  mixed   $from  The date/time value or range.
+     * @param  ?string $until The date/time value.
+     * @throws InvalidArgumentException if range is invalid.
+     */
+    public function __construct($from, $until = null)
+    {
+        if ($until === null) {
+            if (is_string($from)) {
+                list($this->from, $this->until) = static::splitRange($from);
+                return;
+            }
+
+            if (is_array($from)) {
+                list($this->from, $this->until) = $from;
+                return;
+            }
+        }
+
+        if (is_string($from) && is_string($until)) {
+            $this->from  = $from;
+            $this->until = $until;
+            return;
+        }
+
+        throw new InvalidArgumentException('Expected ISO-8601-2 string or array with exactly 2 items');
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string)$this->getRange();
+    }
+
+    /**
+     * @return ?string
+     */
+    public function getFrom()
+    {
+        return $this->from;
+    }
+
+    /**
+     * @return ?string
+     */
+    public function getUntil()
+    {
+        return $this->until;
+    }
+
+    /**
+     * @return ?string
+     */
+    public function getRange()
+    {
+        if ($this->from === null) {
+            return $this->until;
+        }
+
+        if ($this->until === null) {
+            return $this->from;
+        }
+
+        return $this->from . '/' . $this->until;
+    }
+
+    /**
+     * @param  ?string $format
+     * @param  ?string $present
+     * @return string
+     */
+    public function format($format = null, $present = null)
+    {
+        return static::formatRange($this->from, $this->until, $format, $present);
+    }
+
+    /**
+     * @param  array|self $a
+     * @param  array|self $b
+     * @return int
+     */
+    public static function compare($a, $b)
+    {
+        $a = static::create($a);
+        $b = static::create($b);
+
+        $fromA  = static::createDateTime($a->getFrom());
+        $fromB  = static::createDateTime($b->getFrom());
+        $untilA = static::createDateTime($a->getUntil());
+        $untilB = static::createDateTime($b->getUntil());
+
+        // by from (NULL first)
+        if (!$fromA && $fromB) {
+            return -1;
+        } else if ($fromA && !$fromB) {
+            return 1;
+        } else if ($fromA && $fromB) {
+            // ASC by from
+            if ($fromA < $fromB) {
+                return -1;
+            } else if ($fromA > $fromB) {
+                return 1;
+            }
+        }
+
+        // by to (NULL last)
+        if (!$untilA && $untilB) {
+            return 1;
+        } else if ($untilA && !$untilB) {
+            return -1;
+        } else if ($untilA && $untilB) {
+            // ASC by to
+            if ($untilA < $untilB) {
+                return -1;
+            } else if ($untilA > $untilB) {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function splitRange($time)
+    {
+        return array_pad(explode('/', $time), 2, null);
+    }
+
+    /**
+     * @return ?array
+     */
+    public static function splitDate($time)
+    {
+        if (preg_match(static::REGEX_DATE, $time, $parts)) {
+            return $parts;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param  string  $time
+     * @param  ?string $format
+     * @return string
+     */
+    public static function formatDate($time, $format = null)
+    {
+        $parts = static::splitDate($time);
+        if (is_array($parts)) {
+            if (isset($parts['Y'], $parts['m'], $parts['d'])) {
+                $date = new DateTimeImmutable($time);
+                return $date->format($format ?: 'j F Y');
+            } elseif (isset($parts['Y'], $parts['m'])) {
+                $date = new DateTimeImmutable($time);
+                return $date->format($format ?: 'F Y');
+            } elseif (isset($parts['Y'])) {
+                return $parts['Y'];
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param  string  $from
+     * @param  string  $until
+     * @param  ?string $format
+     * @param  ?string $present
+     * @return string
+     */
+    public static function formatRange($from, $until, $format = null, $present = null)
+    {
+        $time = static::formatDate($from, $format);
+        if ($until) {
+            if ($until === static::OPEN_DATE) {
+                $time .= '–' . ($present ? date($present) : 'Ongoing');
+            } else {
+                $time .= '–' . static::formatDate($until, $format);
+            }
+        }
+
+        return $time;
+    }
+
+    /**
+     * @param  string $time A date/time value.
+     * @return ?DateTimeInterface
+     */
+    public static function createDateTime($time)
+    {
+        if ($time instanceof DateTimeInterface) {
+            return $time;
+        }
+
+        if ($time === static::OPEN_DATE) {
+            $time = 'now';
+        }
+
+        return new DateTimeImmutable($time);
+    }
+
+    /**
+     * @param  mixed $range A date/time range.
+     * @return ?static
+     */
+    public static function create($range)
+    {
+        if ($range instanceof self) {
+            return $range;
+        }
+
+        if (is_string($range)) {
+            return new static($range);
+        }
+
+        if (is_array($range) && count($range) === 2) {
+            return new static(...$range);
+        }
+
+        return null;
+    }
+}
+
+class HTML
+{
+    /**
+     * Render an HTML anchor element.
+     *
+     * @param  Value $href The URI of the elemet.
+     * @param  Value $text The text node of the element.
+     * @return string An HTML anchor element.
+     */
+    public static function anchor($href, $text = null)
+    {
+        if (empty($href)) {
+            return $text;
+        }
+
+        if ($text === null) {
+            $host = parse_url($href, PHP_URL_HOST);
+            $text = $href;
+        } else {
+            $host = null;
+        }
+
+        $attr = [
+            'href'     => $href,
+            'hreflang' => static::locale($href),
+            'target'   => null,
+            'rel'      => 'noreferrer noopener',
+            'lang'     => static::locale($text),
+        ];
+
+        if ($host !== null) {
+            $text = $host;
+        }
+
+        $elem = [
+            '%text' => $text,
+            '%attr' => static::attr($attr),
+        ];
+
+        $html = '<a %attr>%text</a>';
+
+        return strtr($html, $elem);
+    }
+
+    /**
+     * Render an HTML time element.
+     *
+     * @param  DateTimeInterface $time The date/time value.
+     * @param  Value             $text The text node of the element.
+     * @return string An HTML time element.
+     */
+    public static function time($time, $text = null)
+    {
+        if ($time instanceof DateRange) {
+            if ($text === null) {
+                $text = $time->format();
+            }
+
+            $time = $time->getRange();
+        } elseif (is_string($time) && $text === null) {
+            $range = DateRange::splitRange($time);
+            $text  = DateRange::formatRange(...$range);
+        }
+
+        $attr = [
+            'datetime' => $time,
+            'lang'     => static::locale($text),
+        ];
+
+        $elem = [
+            '%text' => $text,
+            '%attr' => static::attr($attr),
+        ];
+
+        $html = '<time %attr>%text</time>';
+
+        return strtr($html, $elem);
+    }
+
+    /**
+     * Render an HTML icon element.
+     *
+     * @param  string $icon The icon value.
+     * @return string An HTML span element.
+     */
+    public static function icon($icon)
+    {
+        if (empty($icon)) {
+            return null;
+        }
+
+        $elem = [
+            '%icon' => $icon,
+        ];
+
+        $html = '<span role="img" aria-hidden="true">%icon</span>';
+
+        return strtr($html, $elem);
+    }
+
+    /**
+     * Retrieves a string representing the BCP 47 language tag of the value.
+     *
+     * @param  Value $value A value to resolve.
+     * @return string|null A value that is not the current locale.
+     */
+    protected static function locale($value)
+    {
+        if ($value instanceof Value) {
+            $locale = $value->localeOf();
+            if ($locale !== 'en') {
+                return $locale;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Render an associative array as a string of HTML attributes.
+     *
+     * @param  array $attrs Associative array of HTML attributes.
+     * @return string A string of HTML attributes.
+     */
+    protected static function attr(array $attrs)
+    {
+        $html = '';
+
+        foreach ($attrs as $attr => $val) {
+            if ($val !== null) {
+                if (is_bool($val)) {
+                    $html .= ' ' . $attr;
+                } else {
+                    $val   = htmlspecialchars($val, ENT_QUOTES);
+                    $html .= sprintf(' %s="%s"', $attr, $val);
+                }
+            }
+        }
+
+        return $html;
+    }
+}
---
+++ www/index.php
@@ -0,0 +1,195 @@
+<?php
+
+define('BASEPATH', dirname(__DIR__));
+
+require BASEPATH . '/src/app.php';
+
+$file = BASEPATH . '/src/data.json';
+$json = file_get_contents($file);
+$data = json_decode($json);
+
+if ($data === null) {
+    throw new Error('JSON: '.json_last_error_msg(), json_last_error());
+}
+
+$icons = [ '🌀', '🧠', '☕️', '⚙️', '🛠', '🧰', '🎒', '⌨️', '🖥', '💻', '🌎', '🌑', '🇨🇦', ];
+
+$workRanges = [];
+
+?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Chauncey McAskill</title>
+        <meta content="width=device-width,initial-scale=1" name="viewport">
+
+        <style>
+            body {
+                max-width: 45rem;
+                margin-top: 1.5em;
+                margin-right: 1.5em;
+                margin-bottom: 6em;
+                margin-left: 3em;
+                color: #2C3235;
+                font-family: sans-serif;
+                font-weight: 300;
+                line-height: 1.5;
+            }
+            body > header {
+                min-height: calc(100vh - 5.5em);
+            }
+            body > header > h1 {
+                font-size: 1.5em;
+            }
+            body > header > h1 [role="img"] {
+                display: inline-block;
+                margin-left: -2em;
+                width: 2em;
+                text-align: center;
+                vertical-align: middle;
+            }
+            body > header > p:nth-child(4) {
+                opacity: 0.6;
+            }
+            body > header > p:nth-child(5) {
+                opacity: 0.4;
+            }
+            h1, h2, h3, h4, h5, h6, strong {
+                font-weight: 500;
+            }
+            h1, h2 {
+                font-size: 1.25em;
+            }
+            h3, h4 {
+                font-size: 1.125em;
+            }
+            h5, h6 {
+                font-size: 1em;
+            }
+            ul {
+                padding-left: 0;
+            }
+            li {
+                list-style: none;
+                margin-left: 2em;
+            }
+            li [role="img"] {
+                display: inline-block;
+                margin-left: -2em;
+                width: 2em;
+                vertical-align: middle;
+            }
+            table {
+                border-color: #DFDFDE;
+            }
+            a { color: #00F; }
+            /*
+            ul { padding-left: 0; }
+            li { list-style: none; margin-left: 1em }
+            */
+            @media screen and (prefers-color-scheme: dark) {
+                body {
+                    background-color: #16191B;
+                    color: #E2E6E8;
+                }
+                a { color: #FF0; }
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1><?php echo HTML::icon($icons[mt_rand(0, (count($icons) - 1))]); ?>Chauncey McAskill</h1>
+            <p>Web Developer<br>Montréal, QC</p>
+<?php
+
+            foreach ($data->work as $epoch) {
+                $epoch->name = Value::create($epoch->name);
+                $epoch->url  = Value::create($epoch->url);
+
+                if (!empty($epoch->projects)) {
+                    $workRanges[] = $epoch->period;
+                }
+
+?>
+            <p>
+                <strong><?php echo HTML::anchor($epoch->url, $epoch->name); ?></strong>
+                <br><?php echo HTML::time($epoch->period) . "\n"; ?>
+                <br><small><?php echo implode(', ', $epoch->roles); ?></small>
+            </p>
+<?php
+
+            } // $work
+
+?>
+        </header>
+<?php
+
+        if (count($workRanges)) {
+            $workRanges = new DateRangeCollection(...$workRanges);
+            $workSpan   = $workRanges->getRange();
+
+?>
+
+        <section>
+            <h1>Work<br><?php echo HTML::time($workSpan, $workSpan->format('Y', 'Y')); ?></h1>
+<?php
+
+            foreach ($data->work as $epoch) {
+                if (empty($epoch->projects)) {
+                    continue;
+                }
+
+?>
+
+            <h3><?php echo $epoch->name; ?></h3>
+            <ul>
+<?php
+
+                foreach ($epoch->projects as $project) {
+                    $project->name = Value::create($project->name);
+                    $project->url  = Value::create($project->url);
+
+?>
+                <li>
+                    <p>
+                        <?php echo HTML::icon($project->icon); ?><strong><?php echo HTML::anchor($project->url, $project->name); ?></strong> —
+                        <br><?php echo $project->description . "\n"; ?>
+                        <br><small><?php echo implode(', ', $project->roles); ?> (<?php echo HTML::time($project->period); ?>)</small>
+                    </p>
+                </li>
+<?php
+
+                } // $projects
+
+?>
+            </ul>
+<?php
+
+            } // $work
+
+?>
+        </section>
+<?php
+
+        } // $workRanges
+
+?>
+
+        <section>
+            <h1>Contact</h1>
+            <ul>
+<?php
+
+            foreach ($data->contact as $link) {
+
+?>
+                <li><?php echo HTML::icon($link->icon); ?><?php echo HTML::anchor($link->url, $link->text); ?></li>
+<?php
+
+            } // $contact
+
+?>
+            </ul>
+        </section>
+    </body>
+</html>
